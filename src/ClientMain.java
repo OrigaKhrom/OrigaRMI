dfc7ab645fc4928fb5395edd601e9a37585e6e2f
Origa Khromova <97949327+OrigaKhrom@users.noreply.github.com>
2022-03-30 22:03:48 +0300

    Origa RMI version 1.0

---
+++ src/ClientMain.java
@@ -0,0 +1,27 @@
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class ClientMain {
+    public static final String UNIQUE_BINDING_NAME = "server.calculator";
+
+    public static void main(String[] args) throws RemoteException, NotBoundException {
+
+        final Registry registry = LocateRegistry.getRegistry(2732);
+
+        Calculator calculator = (Calculator) registry.lookup(UNIQUE_BINDING_NAME);  //работа RMI основана на использовании прокси, поэтому удаленный вызов доступен только для методов интерфейсов, а не классов.
+
+        int multiplyResult = calculator.multiply(20, 50);
+
+        System.out.println(multiplyResult);
+    }
+}
+
+
+//В примере выполняются четыре основных действия:
+//
+//определение удаленного интерфейса с объявлениями методов, которые клиент может вызвать у удаленного объекта;
+//определение реализации удаленного объекта для удаленного интерфейса;
+//определение клиентского приложения, которое использует удаленную ссылку, чтобы взаимодействовать с реализацией интерфейса;
+//компиляция и выполнение удаленного объекта и клиента.
