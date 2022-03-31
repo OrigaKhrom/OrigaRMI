e2aec40650a11f75da04c9a85af31f9d04b17dba
Origa Khromova <97949327+OrigaKhrom@users.noreply.github.com>
2022-03-31 13:10:09 +0300

    server number

--- src/ServerMain.java
+++ src/ServerMain.java
@@ -13,7 +13,7 @@ public class ServerMain {
 
         final RemoteCalculationServer server = new RemoteCalculationServer();
 
-        final Registry registry = LocateRegistry.createRegistry(2733);
+        final Registry registry = LocateRegistry.createRegistry(2756);
 
         Remote stub = UnicastRemoteObject.exportObject(server, 0);
         registry.bind(UNIQUE_BINDING_NAME, stub);
